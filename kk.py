e4947b22d1d0c4c8746f88aa4092cefdd7b0ee32
l0gx <42222234+l0gx@users.noreply.github.com>
2018-08-09 19:11:15 +0300

    Update kk.py

--- kk.py
+++ kk.py
@@ -1,27 +1,34 @@
 #!/usr/bin/python
 import random
-print("PRO TOOL      >BETA");
-print("1-random number");
-print("2-random name");
-print("3-random country");
-listc= int(input("chosse a number: "));
-while listc < 1 or listc > 4 :
-  listc= int(input("chosse a number: "));
-if listc > 0 < 4:
-  if listc == 1:
-     min= int(input("chosse a minmum: "));
-     max= int(input("chosse a maxmum: "));
-     print (random.randint(min,max));
-     print("the code is end thanks for trying");
-     while listc < 1 or listc > 4 :
-          listc= int(input("chosse a number: "));
-  if listc == 2:
-     names=["mohammed","ahmed","moustafa","omer","mosa","jack","locas","willim","tomas","netwan"];
-     nm = random.randint(0,9);
-     print(names[nm]);
-     print("the code is end thanks for trying");
-  if listc == 3:
-     countey=["USA","UEA","UK","canada","iraq","jordan","germany","sudia arbaia","moroco","netherland"];
-     nc = random.randint(0,9);
-     print(countey[nc]);
-     print("the code is end thanks for trying");
+s = (" ");
+print(70*s + "-----------------------");
+print(70*s + "|  PRO TOOL" + 7*s + "      >BETA    |");
+print(70*s + "----------------------- \n");
+print("1-Random Number");
+print("2-Random Name");
+print("3-Random Country");
+def numcho():
+  listc= str(input("Choose a number: "));
+  ctf = listc.isdigit();
+  while ctf != True:
+    listc= int(input("Choose a number: "));
+    break
+  while int(listc) < 1 or int(listc) > 4:
+    listc= str(input("Choose a number: "));
+  if int(listc) > 0 < 4:
+    if int(listc) == 1:
+       min= int(input("Choose the minimum number: "));
+       max= int(input("Choose the maxmum number: "));
+       print (random.randint(min,max));
+    if int(listc) == 2:
+       names=["mohammed","ahmed","moustafa","omer","mousa","jack","locas","willim","tomas","netwan"];
+       nm = random.randint(0,9);
+       print(names[nm]);
+       print("the code is end thanks for trying");
+    if int(listc) == 3:
+       countey=["USA","UEA","UK","canada","iraq","jordan","germany","sudia arbaia","moroco","netherland"];
+       nc = random.randint(0,9);
+       print(countey[nc]);
+       print("the code is end thanks for trying");
+while True:
+   numcho();
